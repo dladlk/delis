3a49d63f0601b47bb46de4d509fde6ac95a66ea1
funtusthan <funtushan@gmail.com>
2018-12-19 08:31:27 +0200

    temporary process http results

--- delis-web/src/main/java/dk/erst/delis/rest/api/document/DocumentRestController.java
+++ delis-web/src/main/java/dk/erst/delis/rest/api/document/DocumentRestController.java
@@ -14,10 +14,14 @@ import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ResourceUtils;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.io.*;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,26 +30,37 @@ import java.util.stream.Collectors;
  * @author Iehor Funtusov, created by 18.12.18
  */
 
+@Validated
 @RestController
 @RequestMapping("/rest/document")
 public class DocumentRestController {
 
     @GetMapping
-    public ResponseEntity getDocumentList() {
+    public ResponseEntity getDocumentList(
+            @RequestParam(name = "organisation", required = false) String organisation,
+            @RequestParam(name = "receiver", required = false) String receiver,
+            @RequestParam(name = "status", required = false) DocumentStatus status,
+            @RequestParam(name = "lastError", required = false) DocumentErrorCode lastError,
+            @RequestParam(name = "documentType", required = false) DocumentType documentType,
+            @RequestParam(name = "ingoingFormat", required = false) DocumentFormat ingoingFormat,
+            @RequestParam(name = "received", required = false) String received,
+            @RequestParam(name = "issued", required = false) String issued,
+            @RequestParam(name = "senderName", required = false) String senderName,
+            @RequestParam(name = "receiverName", required = false) String receiverName,
+            @RequestParam(name = "page", defaultValue = "1")
+            @Min(value = 1, message = "must be greater than or equal to 1") int page,
+            @RequestParam(name = "size", defaultValue = "20")
+            @Min(value = 1, message = "must be greater than or equal to 1")
+            @Max(value = 100, message = "should not be greater than or equal to 100") int size) {
 
         File documents = null;
         try {
-            documents = ResourceUtils.getFile("classpath:./static/json/documents.json");
+            documents = ResourceUtils.getFile("classpath:static/json/documents.json");
             InputStream in = new FileInputStream(documents);
             String result = new BufferedReader(new InputStreamReader(in)).lines().collect(Collectors.joining("\n"));
             ObjectMapper mapper = new ObjectMapper();
             TempDocumentList list = mapper.readValue(result, TempDocumentList.class);
-            PageContainer<TempDocument> container = new PageContainer<>();
-            container.setCollectionSize(list.getDocs().size());
-            container.setCurrentPage(1);
-            container.setItems(list.getDocs());
-            container.setPageSize(100);
-            return ResponseEntity.ok(container);
+            return ResponseEntity.ok(getContainer(list.getDocs(), page, size, organisation, receiver, status, lastError, documentType, ingoingFormat, received, issued, senderName, receiverName));
         } catch (FileNotFoundException e) {
             System.out.println("error = " + e.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("not found");
@@ -54,6 +69,46 @@ public class DocumentRestController {
         }
 
         return ResponseEntity.ok("ok");
+    }
+
+    private PageContainer<TempDocument> getContainer(
+            List<TempDocument> documents, int page, int size,
+            String organisation,
+            String receiver,
+            DocumentStatus status,
+            DocumentErrorCode lastError,
+            DocumentType documentType,
+            DocumentFormat ingoingFormat,
+            String received,
+            String issued,
+            String senderName,
+            String receiverName) {
+        PageContainer<TempDocument> container = new PageContainer<>();
+
+        documents = getDocumentsAfterFiltering(documents, page, size, organisation, receiver, status, lastError, documentType, ingoingFormat, received, issued, senderName, receiverName);
+
+        container.setCollectionSize(documents.size());
+        container.setCurrentPage(page);
+        container.setItems(documents);
+        container.setPageSize(size);
+        return container;
+    }
+
+    private List<TempDocument> getDocumentsAfterFiltering(
+            List<TempDocument> documents, int page, int size,
+            String organisation,
+            String receiver,
+            DocumentStatus status,
+            DocumentErrorCode lastError,
+            DocumentType documentType,
+            DocumentFormat ingoingFormat,
+            String received,
+            String issued,
+            String senderName,
+            String receiverName) {
+
+
+        return documents.subList(page - 1, size);
     }
 
     @Getter
