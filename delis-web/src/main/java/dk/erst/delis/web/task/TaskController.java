a745de587f33c955ee3fe74fcf0d5dfdb37e0fde
Dmitriy Lapko <dlk@truelink.dk>
2018-12-26 10:45:33 +0000

    Add empty implementations to TaskController for 5 tasks

--- delis-web/src/main/java/dk/erst/delis/web/task/TaskController.java
+++ delis-web/src/main/java/dk/erst/delis/web/task/TaskController.java
@@ -1,6 +1,7 @@
 package dk.erst.delis.web.task;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,9 +11,35 @@ public class TaskController {
 	public String index() {
 		return "/task/index";
 	}
-	
+
+	@GetMapping("/task/identifierLoad")
+	public String identifierLoad(Model model) {
+		return unimplemented(model);
+	}
+
 	@GetMapping("/task/identifierPublish")
-	public String startIdentifierPublishJob() {
+	public String identifierPublish(Model model) {
+		return unimplemented(model);
+	}
+
+	@GetMapping("/task/documentLoad")
+	public String documentLoad(Model model) {
+		return unimplemented(model);
+	}
+
+	@GetMapping("/task/documentValidate")
+	public String documentValidate(Model model) {
+		return unimplemented(model);
+	}
+
+	@GetMapping("/task/documentDeliver")
+	public String documentDeliver(Model model) {
+		return unimplemented(model);
+	}
+
+	@GetMapping("/task/unimplemented")
+	private String unimplemented(Model model) {
+		model.addAttribute("errorMessage", "Not implemented");
 		return "/task/index";
 	}
 }
