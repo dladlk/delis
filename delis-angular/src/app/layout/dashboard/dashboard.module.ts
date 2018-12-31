07f7632792afc0cd87035cd6597f26e73c90a2f2
funtusthan <funtushan@gmail.com>
2018-12-31 12:24:43 +0200

    remove unwanted components from dashboards

--- delis-angular/src/app/layout/dashboard/dashboard.module.ts
+++ delis-angular/src/app/layout/dashboard/dashboard.module.ts
@@ -7,12 +7,7 @@ import { ChartsModule as Ng2Charts } from "ng2-charts";
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-import {
-    TimelineComponent,
-    NotificationComponent,
-    ChatComponent,
-    ChartDocumentComponent
-} from './components';
+import { ChartDocumentComponent } from './components';
 import { StatModule } from '../../shared';
 import { ErrorsModule } from '../../shared';
 import { ChartDocumentTestGuiStaticService } from "./components/chart-document/services/chart.document.test-gui-static.service";
@@ -31,9 +26,6 @@ import { ChartDocumentTestGuiStaticService } from "./components/chart-document/s
     ],
     declarations: [
         DashboardComponent,
-        TimelineComponent,
-        NotificationComponent,
-        ChatComponent,
         ChartDocumentComponent
     ],
     providers: [ChartDocumentTestGuiStaticService]
