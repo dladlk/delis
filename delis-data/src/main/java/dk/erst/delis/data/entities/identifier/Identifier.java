5b944a09599076622e9026ab4b447d0d723131a8
Andriy.Palval <apal@efisco.net>
2019-01-16 16:18:47 +0200

    Remove redundant to-do

--- delis-data/src/main/java/dk/erst/delis/data/entities/identifier/Identifier.java
+++ delis-data/src/main/java/dk/erst/delis/data/entities/identifier/Identifier.java
@@ -15,7 +15,7 @@ import lombok.Setter;
 @Entity
 @Table(indexes = {
 
-		@Index(name="I_ORGANISAION_ID", columnList="ORGANISATION_ID, STATUS, PUBLISHING_STATUS"), //todo caused com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException: Specified key was too long; max key length is 1000 bytes
+		@Index(name="I_ORGANISAION_ID", columnList="ORGANISATION_ID, STATUS, PUBLISHING_STATUS"),
 		
 		@Index(name="I_ORGANISAION_ID", columnList="LAST_SYNC_ORGANISATION_FACT_ID"),
 
