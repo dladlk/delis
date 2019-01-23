5088f98ffa67c823973de311d4981f81f022c95c
funtusthan <funtushan@gmail.com>
2019-01-23 16:20:44 +0200

    add observable error response from server and view message on layout page

--- delis-angular/src/app/layout/layout.component.ts
+++ delis-angular/src/app/layout/layout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from "@ngx-translate/core";
 import { LocaleService } from "../service/locale.service";
+import {ListenErrorService} from "../service/listen.error.service";
+import {ErrorModel} from "../models/error.model";
 
 @Component({
     selector: 'app-layout',
@@ -11,8 +13,15 @@ export class LayoutComponent implements OnInit {
 
     collapedSideBar: boolean;
 
-    constructor(private translate: TranslateService, private locale: LocaleService) {
+    private error: boolean = false;
+    private errorMessage: string;
+
+    constructor(private translate: TranslateService, private locale: LocaleService, private errorService: ListenErrorService) {
         this.translate.use(locale.getlocale().match(/en|da/) ? locale.getlocale() : 'en');
+        this.errorService.listen().subscribe((listenError: ErrorModel) => {
+            this.error = true;
+            this.errorMessage = 'status is: ' + listenError.status + ' : ' + ' message is: ' + listenError.message;
+        });
     }
 
     ngOnInit() {}
