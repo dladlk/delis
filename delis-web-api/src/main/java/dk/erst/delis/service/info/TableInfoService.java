476819fc0a935f8b69b8112fed4e57517c5a6165
funtusthan <funtushan@gmail.com>
2019-01-25 17:40:40 +0200

    fix table-info

--- delis-web-api/src/main/java/dk/erst/delis/service/info/TableInfoService.java
+++ delis-web-api/src/main/java/dk/erst/delis/service/info/TableInfoService.java
@@ -1,19 +1,17 @@
 package dk.erst.delis.service.info;
 
-import com.google.common.reflect.ClassPath;
-
 import dk.erst.delis.data.annotations.WebApiContent;
+import dk.erst.delis.data.entities.document.Document;
+import dk.erst.delis.data.entities.journal.JournalDocument;
 import dk.erst.delis.rest.data.response.ListContainer;
 import dk.erst.delis.rest.data.response.info.TableInfoData;
 
 import lombok.extern.slf4j.Slf4j;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.ObjectUtils;
 
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.*;
@@ -27,40 +25,32 @@ import java.util.stream.Collectors;
 @Service
 public class TableInfoService {
 
-    private static final String BASE_ENTITY_PACKAGE = "dk.erst.delis.data.entities";
-
     public ListContainer<TableInfoData> getTableInfoByAllEntities() {
 
-        List<String> packages = Arrays.stream(
-                Package.getPackages())
-                .map(Package::getName)
-                .filter(pack -> pack.startsWith(BASE_ENTITY_PACKAGE))
-                .filter(pack -> ObjectUtils.notEqual(pack, BASE_ENTITY_PACKAGE))
-                .collect(Collectors.toList());
-
-        final ClassLoader loader = Thread.currentThread().getContextClassLoader();
         List<Class> entityClasses = new ArrayList<>();
 
-        packages.forEach(pack -> {
-            try {
-                for (ClassPath.ClassInfo info : ClassPath.from(loader).getTopLevelClasses()) {
-                    if (info.getName().startsWith(pack)) {
-                        Class<?> entity = info.load();
-                        if (Objects.nonNull(entity.getAnnotation(WebApiContent.class))) {
-                            entityClasses.add(entity);
-                        }
-                    }
-                }
-            } catch (IOException e) {
-                log.error("problem scan package: " + e.getMessage());
+        Arrays.asList(Document.class, JournalDocument.class).forEach(entityClass -> {
+            if (Objects.nonNull(entityClass.getAnnotation(WebApiContent.class))) {
+                entityClasses.add(entityClass);
             }
         });
 
-        return new ListContainer(generateTableInfoDataList(entityClasses).stream().filter(entity -> CollectionUtils.isNotEmpty(entity.getEntityEnumInfo().keySet())).collect(Collectors.toList()));
+        if (CollectionUtils.isNotEmpty(entityClasses)) {
+            return new ListContainer<>(
+                    generateTableInfoDataList(entityClasses)
+                            .stream()
+                            .filter(entity -> CollectionUtils.isNotEmpty(entity.getEntityEnumInfo().keySet()))
+                            .collect(Collectors.toList()));
+        } else {
+            return new ListContainer<>(Collections.emptyList());
+        }
     }
 
-    private List<TableInfoData> generateTableInfoDataList(List<Class> enities) {
-        return enities.stream().map(this::generateTableInfoData).collect(Collectors.toList());
+    private List<TableInfoData> generateTableInfoDataList(List<Class> entities) {
+        return entities
+                .stream()
+                .map(this::generateTableInfoData)
+                .collect(Collectors.toList());
     }
 
     private TableInfoData generateTableInfoData(Class entity) {
@@ -70,7 +60,12 @@ public class TableInfoService {
         for ( Field field : fields ) {
             if (Modifier.isPrivate(field.getModifiers())) {
                 if (Enum.class.isAssignableFrom(field.getType())) {
-                    entityEnumInfo.put(field.getName(), Arrays.stream(((Class<Enum>) field.getType()).getEnumConstants()).map(Enum::name).collect(Collectors.toList()));
+                    entityEnumInfo.put(
+                            field.getName(),
+                            Arrays
+                                    .stream(((Class<Enum>) field.getType()).getEnumConstants())
+                                    .map(Enum::name)
+                                    .collect(Collectors.toList()));
                 }
             }
         }
