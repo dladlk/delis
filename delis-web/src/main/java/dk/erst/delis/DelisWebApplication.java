96f55fc348f3f3fbb8d938763db553b0f393ce48
funtusthan <funtushan@gmail.com>
2019-02-05 15:04:57 +0200

    add TaskScheduler to delis-web

--- delis-web/src/main/java/dk/erst/delis/DelisWebApplication.java
+++ delis-web/src/main/java/dk/erst/delis/DelisWebApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @EntityScan("dk.erst.delis.data")
 @EnableJpaAuditing
