1de240dc6d7c0c7e4f37b418d6a6923734b364a9
Dmitriy Lapko <dlk@truelink.dk>
2019-02-08 08:41:27 +0000

    Fast fix of strange logic in oxalis-document-sniffer - it does not handle correct BIS3 with schemeId=0088, but requires schemeId=GLN.

---
+++ delis-sbdh/src/main/java/dk/erst/delis/document/sbdh/ubl/DelisInvoiceDocumentParser.java
@@ -0,0 +1,37 @@
+package dk.erst.delis.document.sbdh.ubl;
+
+import no.difi.oxalis.sniffer.document.PlainUBLParser;
+import no.difi.vefa.peppol.common.model.ParticipantIdentifier;
+
+public class DelisInvoiceDocumentParser extends DelisAbstractDocumentParser {
+
+    public DelisInvoiceDocumentParser(PlainUBLParser parser)  {
+        super(parser);
+    }
+
+    @Override
+    public ParticipantIdentifier getSender() {
+        String endpointFirst = "//cac:AccountingSupplierParty/cac:Party/cbc:EndpointID";
+        String companySecond = "//cac:AccountingSupplierParty/cac:Party/cac:PartyLegalEntity/cbc:CompanyID";
+        ParticipantIdentifier s;
+        try {
+            s = participantId(endpointFirst);
+        } catch (IllegalStateException e) {
+            s = participantId(companySecond);
+        }
+        return s;
+    }
+
+    @Override
+    public ParticipantIdentifier getReceiver() {
+        String endpointFirst = "//cac:AccountingCustomerParty/cac:Party/cbc:EndpointID";
+        String companySecond = "//cac:AccountingCustomerParty/cac:Party/cac:PartyLegalEntity/cbc:CompanyID";
+        ParticipantIdentifier s;
+        try {
+            s = participantId(endpointFirst);
+        } catch (IllegalStateException e) {
+            s = participantId(companySecond);
+        }
+        return s;
+    }
+}
