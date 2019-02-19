ee20eaa9089552c6a8dd45769ec87aa95ab6a445
funtusthan <funtushan@gmail.com>
2019-02-19 10:35:41 +0200

    modify SpecificationUtil

--- delis-web-api/src/main/java/dk/erst/delis/util/SpecificationUtil.java
+++ delis-web-api/src/main/java/dk/erst/delis/util/SpecificationUtil.java
@@ -32,53 +32,47 @@ public class SpecificationUtil {
             Root<? extends AbstractEntity> root,
             CriteriaBuilder criteriaBuilder) {
         String containsLikePattern;
-        List<Field> fieldsFromEntityModel = new ArrayList<>();
-        fieldsFromEntityModel.addAll(Arrays.asList(entityClass.getDeclaredFields()));
-        fieldsFromEntityModel.addAll(Arrays.asList(entityClass.getSuperclass().getDeclaredFields()));
-        fieldsFromEntityModel.addAll(Arrays.asList(entityClass.getSuperclass().getSuperclass().getDeclaredFields()));
-        for (Field field : fieldsFromEntityModel) {
+        for (Field field : ClassLoaderUtil.getAllFieldsByEntity(entityClass)) {
             if (Modifier.isPrivate(field.getModifiers())) {
                 String parameter = request.getParameter(field.getName());
-                if (Objects.isNull(parameter) || Objects.equals(parameter, "ALL")) {
-                    continue;
-                }
-                if (field.getType().isPrimitive()) {
-                    if (field.getType().isAssignableFrom(boolean.class)) {
-                        predicates.add(criteriaBuilder.equal(root.get(field.getName()), Boolean.parseBoolean(parameter)));
-                    }
-                } else {
-                    if (field.getType().isAssignableFrom(String.class)) {
-                        containsLikePattern = StringPatternUtil.getContainsLikePattern(parameter);
-                        predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get(field.getName())), containsLikePattern));
-                    }
-                    if (Enum.class.isAssignableFrom(field.getType())) {
-                        predicates.add(criteriaBuilder.equal(root.get(field.getName()), Enum.valueOf((Class<Enum>) field.getType(), parameter)));
-                    }
-                    if (field.getType().getSuperclass().isAssignableFrom(AbstractEntity.class)
-                            || field.getType().getSuperclass().isAssignableFrom(AbstractCreateUpdateEntity.class)
-                            || field.getType().getSuperclass().isAssignableFrom(AbstractCreateEntity.class)) {
-                        List<Predicate> innerEntitiesPredicates = new ArrayList<>();
-                        List<Field> innerEntitiesFields = new ArrayList<>(Arrays.asList(field.getType().getDeclaredFields()));
-                        for (Field innerField : innerEntitiesFields) {
-                            if (Modifier.isPrivate(innerField.getModifiers())) {
-                                if (innerField.getType().isAssignableFrom(String.class)) {
-                                    containsLikePattern = StringPatternUtil.getContainsLikePattern(parameter);
-                                    innerEntitiesPredicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get(field.getName()).get(innerField.getName())), containsLikePattern));
+                if (Objects.nonNull(parameter)) {
+                    if (field.getType().isPrimitive()) {
+                        if (field.getType().isAssignableFrom(boolean.class)) {
+                            predicates.add(criteriaBuilder.equal(root.get(field.getName()), Boolean.parseBoolean(parameter)));
+                        }
+                    } else {
+                        if (field.getType().isAssignableFrom(String.class)) {
+                            containsLikePattern = StringPatternUtil.getContainsLikePattern(parameter);
+                            predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get(field.getName())), containsLikePattern));
+                        }
+                        if (Enum.class.isAssignableFrom(field.getType())) {
+                            predicates.add(criteriaBuilder.equal(root.get(field.getName()), Enum.valueOf((Class<Enum>) field.getType(), parameter)));
+                        }
+                        if (field.getType().getSuperclass().isAssignableFrom(AbstractEntity.class)
+                                || field.getType().getSuperclass().isAssignableFrom(AbstractCreateUpdateEntity.class)
+                                || field.getType().getSuperclass().isAssignableFrom(AbstractCreateEntity.class)) {
+                            List<Predicate> innerEntitiesPredicates = new ArrayList<>();
+                            for (Field innerField : ClassLoaderUtil.getAllFieldsByEntity(field.getType())) {
+                                if (Modifier.isPrivate(innerField.getModifiers())) {
+                                    if (innerField.getType().isAssignableFrom(String.class)) {
+                                        containsLikePattern = StringPatternUtil.getContainsLikePattern(parameter);
+                                        innerEntitiesPredicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get(field.getName()).get(innerField.getName())), containsLikePattern));
+                                    }
                                 }
                             }
+                            if (CollectionUtils.isNotEmpty(innerEntitiesPredicates)) {
+                                predicates.add(criteriaBuilder.or(innerEntitiesPredicates.toArray(new Predicate[0])));
+                            }
                         }
-                        if (CollectionUtils.isNotEmpty(innerEntitiesPredicates)) {
-                            predicates.add(criteriaBuilder.or(innerEntitiesPredicates.toArray(new Predicate[0])));
+                        if (field.getType().isAssignableFrom(Date.class)) {
+                            if (StringPatternUtil.dateRegExPattern(parameter)) {
+                                DateRangeModel range = WebRequestUtil.generateDateRange(parameter);
+                                predicates.add(criteriaBuilder.between(root.get(field.getName()), range.getStart(), range.getEnd()));
+                            }
                         }
-                    }
-                    if (field.getType().isAssignableFrom(Date.class)) {
-                        if (StringPatternUtil.dateRegExPattern(parameter)) {
-                            DateRangeModel range = WebRequestUtil.generateDateRange(parameter);
-                            predicates.add(criteriaBuilder.between(root.get(field.getName()), range.getStart(), range.getEnd()));
+                        if (Number.class.isAssignableFrom(field.getType())) {
+                            predicates.add(criteriaBuilder.equal(root.get(field.getName()), parameter));
                         }
-                    }
-                    if (Number.class.isAssignableFrom(field.getType())) {
-                        predicates.add(criteriaBuilder.equal(root.get(field.getName()), parameter));
                     }
                 }
             }
