6c4513e22795f5d741bd0a9f7a13a241e6b9bf2b
Alexandr Geta <aget@efisco.net>
2019-02-25 16:44:51 +0200

    Run test with all XSLT files

--- delis-web/src/test/java/dk/erst/delis/task/document/parse/XSLTMemoryMeasureTool.java
+++ delis-web/src/test/java/dk/erst/delis/task/document/parse/XSLTMemoryMeasureTool.java
@@ -1,13 +1,13 @@
 package dk.erst.delis.task.document.parse;
 
+import dk.erst.delis.task.document.parse.cachingtransformerfactory.DelisTransformerFactory;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
 import java.io.FileInputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import javax.xml.transform.TransformerFactory;
-
-import dk.erst.delis.task.document.parse.XSLTUtil;
-import dk.erst.delis.task.document.parse.cachingtransformerfactory.DelisTransformerFactory;
 
 public class XSLTMemoryMeasureTool {
 
@@ -17,32 +17,39 @@ public class XSLTMemoryMeasureTool {
 	}
 
 	public void test() throws Exception {
-		Path path1 = Paths.get("../delis-resources/transformation/bis3_to_oioubl/v_2018-03-14_34841/BIS-Billing-INV_2_OIOUBL_INV.xslt");
-		Path path2 = Paths.get("../delis-resources/validation/sch/bis3/cen_2018-03-15_1/CEN-EN16931-UBL.xslt");
-//        testTransformerCreation(path1, 10);
-		testTransformerCreation(path2, 10);
-
+		Path baseDir = Paths.get("delis-resources");
+		int creationTimes = 10;
+		Files.walk(baseDir)
+				.filter(p -> p.toString().endsWith(".xslt"))
+				.forEach(path -> {
+					testTransformerCreation(path.toAbsolutePath(), creationTimes);
+				});
 	}
 
-	private void testTransformerCreation(Path xslFilePath, int creationTimes) throws Exception {
+	private void testTransformerCreation(Path xslFilePath, int creationTimes) {
 		boolean withCache = true;
+		System.gc();
 		long usedMemoryBefore = getUsedMemory();
-
 		System.out.println("Run with cache = " + withCache + " on " + xslFilePath);
 		System.out.println("Used Memory before: " + formatMemory(usedMemoryBefore));
 
-		TransformerFactory transformerFactory = DelisTransformerFactory.newInstance(withCache);
-
-		long start = System.currentTimeMillis();
-		for (int i = 0; i < creationTimes; i++) {
-			long startCase = System.currentTimeMillis();
-			XSLTUtil.buildTransformer(new FileInputStream(xslFilePath.toFile()), xslFilePath.toAbsolutePath(), transformerFactory);
-			System.out.println("Case " + i + " " + (System.currentTimeMillis() - startCase) + " ms, " + formatMemory(getUsedMemory()));
+		try {
+			TransformerFactory transformerFactory = DelisTransformerFactory.newInstance(withCache);
+			long start = System.currentTimeMillis();
+			for (int i = 0; i < creationTimes; i++) {
+                long startCase = System.currentTimeMillis();
+                Transformer transformer = XSLTUtil.buildTransformer(new FileInputStream(xslFilePath.toFile()), xslFilePath.toAbsolutePath(), transformerFactory);
+                System.out.println("Case " + i + " " + (System.currentTimeMillis() - startCase) + " ms, " + formatMemory(getUsedMemory()));
+            }
+			long finish = System.currentTimeMillis();
+			long usedMemoryAfter = getUsedMemory();
+			System.out.println(xslFilePath.getFileName());
+			System.out.println("Time spent: " + (finish - start));
+			System.out.println("Memory used: " +  formatMemory(usedMemoryAfter - usedMemoryBefore));
+			System.out.println("===================================");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		long finish = System.currentTimeMillis();
-		long usedMemoryAfter = getUsedMemory();
-		System.out.println("Memory increased: " + formatMemory(usedMemoryAfter - usedMemoryBefore));
-		System.out.println("Time spent: " + (finish - start));
 	}
 
 	private String formatMemory(long s) {
@@ -51,7 +58,6 @@ public class XSLTMemoryMeasureTool {
 
 	private long getUsedMemory() {
 		Runtime runtime = Runtime.getRuntime();
-		runtime.gc();
 		return runtime.totalMemory() - runtime.freeMemory();
 	}
 }
