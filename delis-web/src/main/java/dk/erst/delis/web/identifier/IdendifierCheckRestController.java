3548c282aedfbc6bbad1c4777fe3bcbf816219cb
Andriy.Palval <apal@efisco.net>
2019-03-07 13:55:12 +0200

    Add REST service to check document identifier

---
+++ delis-web/src/main/java/dk/erst/delis/web/identifier/IdendifierCheckRestController.java
@@ -0,0 +1,36 @@
+package dk.erst.delis.web.identifier;
+
+import dk.erst.delis.data.entities.identifier.Identifier;
+import dk.erst.delis.data.enums.identifier.IdentifierStatus;
+import dk.erst.delis.task.identifier.resolve.IdentifierResolverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class IdendifierCheckRestController {
+
+    @Autowired
+    private IdentifierResolverService identifierResolverService;
+
+    @RequestMapping(value = "/testReciever", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity checkIdentifier (@PathVariable("identifier") String compoundIdentifier) {
+        System.out.println("compoundIdentifier = " + compoundIdentifier);
+
+        String[] split = compoundIdentifier.split(":");
+        String type = split[0];
+        String id = split[1];
+
+        Identifier identifier = identifierResolverService.resolve(type, id);
+
+        HttpStatus status = HttpStatus.OK;
+
+        if (identifier == null || identifier.getStatus() == IdentifierStatus.DELETED) {
+            status = HttpStatus.NO_CONTENT;
+        }
+
+        return new ResponseEntity(status);
+    }
+}
