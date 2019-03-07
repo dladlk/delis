98d783a0d83f053f08026a542ffb09ad7a7f8e16
Alexandr Geta <aget@efisco.net>
2019-03-07 17:50:53 +0200

    Load identifiers from file task scheduler

--- delis-web/src/main/java/dk/erst/delis/task/identifier/load/OrganizationIdentifierLoadReport.java
+++ delis-web/src/main/java/dk/erst/delis/task/identifier/load/OrganizationIdentifierLoadReport.java
@@ -3,6 +3,11 @@ package dk.erst.delis.task.identifier.load;
 public class OrganizationIdentifierLoadReport {
 
     private String organizationCode;
+    private int add;
+    private int update;
+    private int delete;
+    private int equal;
+    private int failed;
 
     public String getOrganizationCode() {
         return organizationCode;
@@ -10,5 +15,52 @@ public class OrganizationIdentifierLoadReport {
 
     public void setOrganizationCode(String organizationCode) {
         this.organizationCode = organizationCode;
+    }
+
+
+    public int getAdd() {
+        return add;
+    }
+
+    public void setAdd(int add) {
+        this.add = add;
+    }
+
+    public int getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(int update) {
+        this.update = update;
+    }
+
+    public int getDelete() {
+        return delete;
+    }
+
+    public void setDelete(int delete) {
+        this.delete = delete;
+    }
+
+    public int getEqual() {
+        return equal;
+    }
+
+    public void setEqual(int equal) {
+        this.equal = equal;
+    }
+
+    public int getFailed() {
+        return failed;
+    }
+
+    public void setFailed(int failed) {
+        this.failed = failed;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Organization %s: added %d; updated %d; delete %d; equal %d; failed %d.",
+                organizationCode, add, update, delete, equal, failed);
     }
 }
