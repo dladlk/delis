8064bfb7b5bbe409fd0352678122c9896125d2f9
Andriy.Palval <apal@efisco.net>
2019-03-14 14:23:56 +0200

    Add 'action' check for incoming document check service

--- delis-web/src/main/java/dk/erst/delis/rest/IdentifierCheckRestController.java
+++ delis-web/src/main/java/dk/erst/delis/rest/IdentifierCheckRestController.java
@@ -1,9 +1,13 @@
 package dk.erst.delis.rest;
 
 import dk.erst.delis.data.entities.identifier.Identifier;
+import dk.erst.delis.data.entities.organisation.Organisation;
 import dk.erst.delis.data.enums.identifier.IdentifierStatus;
 import dk.erst.delis.document.sbdh.Main;
 import dk.erst.delis.task.identifier.resolve.IdentifierResolverService;
+import dk.erst.delis.task.organisation.setup.OrganisationSetupService;
+import dk.erst.delis.task.organisation.setup.data.OrganisationSetupData;
+import dk.erst.delis.task.organisation.setup.data.OrganisationSubscriptionProfileGroup;
 import io.swagger.annotations.Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.Date;
+import java.util.Set;
 
 @Api
 @RestController
@@ -26,6 +31,9 @@ public class IdentifierCheckRestController {
 
     @Autowired
     private IdentifierResolverService identifierResolverService;
+
+    @Autowired
+    private OrganisationSetupService organisationSetupService;
 
     @RequestMapping(value = "/receivercheck/{identifier}/{service}/{action}", method = RequestMethod.GET)
     @ResponseBody
@@ -54,7 +62,7 @@ public class IdentifierCheckRestController {
 
         status = checkService(identifier, service, status);
 
-        status = checkAction(identifier, service, status);
+        status = checkAction(identifier, action, status);
 
         long stopTime = new Date().getTime();
         log.info("Stop checkIdentifier. Execution time: " + (stopTime - startTime) * 1000 + " seconds.");
@@ -65,32 +73,64 @@ public class IdentifierCheckRestController {
     private Identifier getIdentifier(@PathVariable("identifier") String compoundIdentifier) {
         Identifier identifier = null;
 
-        String[] split = compoundIdentifier.split(":");
-        String type = split[0];
-        String id = split[1];
+        int index = compoundIdentifier.lastIndexOf(":");
+        String type = compoundIdentifier.substring(0, index);
+        String id = compoundIdentifier.substring(index+1);
 
         identifier = identifierResolverService.resolve(type, id);
         return identifier;
     }
 
     private HttpStatus checkIdentifier(Identifier identifier, HttpStatus status) {
+        log.info("Check identifier");
         if (identifier == null) {
             log.info("Identifier does not exists");
-            status = HttpStatus.NO_CONTENT;
+            status = setFailedStatus();
         } else if (identifier.getStatus() == IdentifierStatus.DELETED) {
             log.info("Identifier marked as deleted");
-            status = HttpStatus.NO_CONTENT;
+            status = setFailedStatus();
         }
+        log.info("Identifier ok");
         return status;
+    }
+
+    private HttpStatus setFailedStatus() {
+        return HttpStatus.NO_CONTENT;
     }
 
     private HttpStatus checkService(Identifier identifier, String service, HttpStatus status) {
+        log.info("Check service");
         //todo
+        log.info("Service ok");
         return status;
     }
 
-    private HttpStatus checkAction(Identifier identifier, String service, HttpStatus status) {
-        //todo
+    private HttpStatus checkAction(Identifier identifier, String action, HttpStatus status) {
+        log.info("Check action: " + action);
+        Organisation organisation = identifier.getOrganisation();
+        OrganisationSetupData setupData = organisationSetupService.load(organisation);
+        Set<OrganisationSubscriptionProfileGroup> profileSet = setupData.getSubscribeProfileSet();
+
+        boolean found = false;
+        for(OrganisationSubscriptionProfileGroup profileGrout : profileSet) {
+            String[] documentIdentifiers = profileGrout.getDocumentIdentifiers();
+            for (String documentId : documentIdentifiers) {
+                if (documentId.equalsIgnoreCase(action)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+                break;
+            }
+        }
+
+        if (!found) {
+            log.info("Action '" + action + "' not found");
+            status = setFailedStatus();
+        }
+
+        log.info("Action check done");
         return status;
     }
 }
--- delis-web/src/test/java/dk/erst/delis/task/identifier/rest/IdentifierCheckTestIT.java
+++ delis-web/src/test/java/dk/erst/delis/task/identifier/rest/IdentifierCheckTestIT.java
@@ -1,8 +1,8 @@
 package dk.erst.delis.task.identifier.rest;
 
+import dk.erst.delis.rest.IdentifierCheckRestController;
 import dk.erst.delis.task.identifier.load.IdentifierLoadService;
 import dk.erst.delis.task.identifier.load.IdentifierLoadServiceTestIT;
-import dk.erst.delis.rest.IdentifierCheckRestController;
 import dk.erst.delis.web.organisation.OrganisationService;
 import org.junit.Before;
 import org.junit.Test;
