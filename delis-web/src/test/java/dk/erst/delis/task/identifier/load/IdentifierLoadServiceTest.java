dbb63f46eaed7fd1716eaa17eda8fd9843d4a313
Andriy.Palval <apal@efisco.net>
2019-03-15 14:07:31 +0200

    Fix test annotation

--- delis-web/src/test/java/dk/erst/delis/task/identifier/load/IdentifierLoadServiceTest.java
+++ delis-web/src/test/java/dk/erst/delis/task/identifier/load/IdentifierLoadServiceTest.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +24,7 @@ import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
-@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class IdentifierLoadServiceTest {
 
     @Autowired
