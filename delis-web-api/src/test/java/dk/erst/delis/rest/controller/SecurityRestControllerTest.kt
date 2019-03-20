31ea1cecab68c7e13953b71946fafa8f3c97a78b
funtusthan <funtushan@gmail.com>
2019-03-20 13:59:06 +0200

    add SecurityRestControllerTest

---
+++ delis-web-api/src/test/java/dk/erst/delis/rest/controller/SecurityRestControllerTest.kt
@@ -0,0 +1,111 @@
+package dk.erst.delis.rest.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+import dk.erst.delis.DelisWebApiApplication
+import dk.erst.delis.data.entities.user.User
+import dk.erst.delis.exception.handler.RestExceptionHandler
+import dk.erst.delis.rest.controller.auth.SecurityRestController
+import dk.erst.delis.rest.data.request.login.LoginData
+import dk.erst.delis.rest.data.response.DataContainer
+import dk.erst.delis.rest.data.response.auth.AuthData
+import dk.erst.delis.service.auth.AuthService
+import dk.erst.delis.service.auth.AuthUserProviderService
+
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.mockito.MockitoAnnotations
+
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.web.servlet.MvcResult
+
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+
+/**
+ * @author funtusthan, created by 19.03.19
+ */
+
+@RunWith(SpringRunner::class)
+@DataJpaTest
+@SpringBootTest(classes = [DelisWebApiApplication::class])
+@ComponentScan(basePackages = ["dk.erst.delis"])
+@TestPropertySource(properties = ["spring.jpa.hibernate.ddl-auto=update"])
+@AutoConfigureMockMvc
+open class SecurityRestControllerTest {
+
+    private val baseUrl = "/rest/security/signin"
+    private val username = "admin"
+    private val password = "admin"
+
+    private lateinit var mvc: MockMvc
+    private lateinit var src: SecurityRestController
+
+    @Autowired private lateinit var em: TestEntityManager
+    @Autowired private lateinit var authService: AuthService
+    @Autowired private lateinit var authUserProviderService: AuthUserProviderService
+
+    @Before
+    fun init() {
+        MockitoAnnotations.initMocks(this)
+        this.src = SecurityRestController(authService, authUserProviderService)
+        this.mvc = MockMvcBuilders.standaloneSetup(this.src).setControllerAdvice(RestExceptionHandler()).build()
+    }
+
+    @Before
+    fun testUser() {
+        var user = User()
+        user.username = username
+        user.password = BCryptPasswordEncoder().encode(password)
+        em.persist(user)
+        user = em.find(User::class.java, 1L)
+        assertEquals(user.username, username)
+    }
+
+    @Test
+    fun testLogin() {
+
+        val loginData = LoginData()
+        loginData.login = username
+        loginData.password = password
+
+        val mvcResult: MvcResult = this.mvc.perform(MockMvcRequestBuilders.post(baseUrl)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(ObjectMapper().writeValueAsString(loginData)))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andReturn()
+
+        assertEquals(200, mvcResult.response.status)
+
+        val data: DataContainer<AuthData> = Gson().fromJson(mvcResult.response.contentAsString,
+                object : TypeToken<DataContainer<AuthData>>() {}.type)
+
+        val authData: AuthData = data.data
+
+        assertEquals(authData.username, username)
+
+        val regex = """\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b""".toRegex() // UUIDs regex
+
+        assertTrue(regex.containsMatchIn(authData.token))
+    }
+}
