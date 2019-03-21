ed9300227782b6a8b2df766ac5d56e450e0ad3cb
funtusthan <funtushan@gmail.com>
2019-03-21 15:06:02 +0200

    fix login page

--- delis-angular/src/app/login/login.component.ts
+++ delis-angular/src/app/login/login.component.ts
@@ -20,7 +20,8 @@ export class LoginComponent implements OnInit {
     login: string;
     password: string;
 
-    error: boolean;
+    message: string;
+    errorStatus: boolean;
 
     constructor(
         private tokenService: TokenService,
@@ -30,24 +31,47 @@ export class LoginComponent implements OnInit {
         private contentSelectInfoService: ContentSelectInfoService,
         public router: Router) {
         this.translate.use(locale.getlocale().match(/en|da/) ? locale.getlocale() : 'en');
+        this.errorStatus = false;
     }
 
     ngOnInit() {
     }
 
     onLoggedin() {
+        if (this.login === undefined && this.password === undefined) {
+            this.message = "login and password can not be empty";
+            this.errorStatus = true;
+            return;
+        }
+        if (this.login === undefined) {
+            this.message = "login can not be empty";
+            this.errorStatus = true;
+            return;
+        }
+        if (this.password === undefined) {
+            this.message = "password can not be empty";
+            this.errorStatus = true;
+            return;
+        }
         this.auth.login(this.login, this.password).subscribe(
             (data: {}) => {
                 let loginData: LoginData = data["data"];
                 this.tokenService.setToken(loginData.token);
                 localStorage.setItem('username', loginData.username);
-                localStorage.setItem('ngxDaterangepickerMdInit', 'true');
                 this.contentSelectInfoService.generateAllContentSelectInfo();
                 this.contentSelectInfoService.generateUniqueOrganizationNameInfo();
                 this.contentSelectInfoService.generateDateRangeInfo();
+                this.errorStatus = false;
                 this.router.navigate(['/dashboard']);
             }, error => {
-                this.error = true;
+                console.log('error : ' + error.status);
+                this.errorStatus = true;
+                if (error.status === 0) {
+                    this.message = "server disconnect";
+                }
+                if (error.status === 401) {
+                    this.message = "invalid username or password";
+                }
                 this.router.navigate(['/login']);
             }
         );
