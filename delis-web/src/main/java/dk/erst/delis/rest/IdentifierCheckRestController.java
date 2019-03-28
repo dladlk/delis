3b83b168c1060d12977c9f72853ec40076b388b3
Andriy.Palval <apal@efisco.net>
2019-03-28 13:15:39 +0200

    Make identifier check rest service open

--- delis-web/src/main/java/dk/erst/delis/rest/IdentifierCheckRestController.java
+++ delis-web/src/main/java/dk/erst/delis/rest/IdentifierCheckRestController.java
@@ -22,7 +22,7 @@ import java.util.Set;
 
 @Api
 @RestController
-@RequestMapping("rest")
+@RequestMapping("/openrest")
 public class IdentifierCheckRestController {
 
     private static final Logger log = LoggerFactory.getLogger(IdentifierResolverService.class);
