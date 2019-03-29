8c94e16274ae0677dcafdffc72a03a7d9f7f4324
Andriy.Palval <apal@efisco.net>
2019-03-29 18:29:12 +0200

    Organize imports

--- delis-web/src/main/java/dk/erst/delis/rest/IdentifierCheckRestController.java
+++ delis-web/src/main/java/dk/erst/delis/rest/IdentifierCheckRestController.java
@@ -13,12 +13,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Set;
 
 @Api
