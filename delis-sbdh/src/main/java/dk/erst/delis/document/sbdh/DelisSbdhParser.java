901a212859d6f7032124b99bdf3222bfcc356e81
Alexandr Geta <aget@efisco.net>
2019-04-11 18:50:35 +0300

    Added DelisUBLHeaderParser which returns default value for ublVersion = 2.1

--- delis-sbdh/src/main/java/dk/erst/delis/document/sbdh/DelisSbdhParser.java
+++ delis-sbdh/src/main/java/dk/erst/delis/document/sbdh/DelisSbdhParser.java
@@ -3,6 +3,7 @@ package dk.erst.delis.document.sbdh;
 import dk.erst.delis.document.sbdh.cii.CIIHeaderParser;
 import dk.erst.delis.document.sbdh.cii.CIINapeSpaceResolver;
 import dk.erst.delis.document.sbdh.ubl.DelisInvoiceDocumentParser;
+import dk.erst.delis.document.sbdh.ubl.DelisUBLHeaderParser;
 import no.difi.oxalis.sniffer.PeppolStandardBusinessHeader;
 import no.difi.oxalis.sniffer.document.HardCodedNamespaceResolver;
 import no.difi.oxalis.sniffer.document.PlainUBLHeaderParser;
@@ -21,103 +22,103 @@ import java.io.InputStream;
 
 public class DelisSbdhParser {
 
-	private static final DocumentBuilderFactory documentBuilderFactory;
+    private static final DocumentBuilderFactory documentBuilderFactory;
 
-	static {
-		documentBuilderFactory = DocumentBuilderFactory.newInstance();
-		documentBuilderFactory.setNamespaceAware(true);
+    static {
+        documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        documentBuilderFactory.setNamespaceAware(true);
 
-		try {
-			documentBuilderFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-		} catch (ParserConfigurationException e) {
-			throw new IllegalStateException("Unable to configure DOM parser for secure processing.", e);
-		}
-	}
+        try {
+            documentBuilderFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+        } catch (ParserConfigurationException e) {
+            throw new IllegalStateException("Unable to configure DOM parser for secure processing.", e);
+        }
+    }
 
-	public DelisSbdhParser() {
-		/*
-		 * Overwrite constructor to surpress NoSbdhParser log warning
+    public DelisSbdhParser() {
+        /*
+         * Overwrite constructor to surpress NoSbdhParser log warning
 		 */
-	}
+    }
 
-	public Header parse(InputStream inputStream) {
-		return originalParse(inputStream).toVefa();
-	}
+    public Header parse(InputStream inputStream) {
+        return originalParse(inputStream).toVefa();
+    }
 
-	public PeppolStandardBusinessHeader originalParse(InputStream inputStream) {
-		try {
-			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-			Document document = documentBuilder.parse(inputStream);
-			if(hasSBDH(document)){
-				throw new AlreadySBDHException("Document already have SBDH.");
-			}
-			boolean isCII = isCrossIndustryInvoice(document);
-			XPath xPath = XPathFactory.newInstance().newXPath();
-			NamespaceContext nsContext = new HardCodedNamespaceResolver();
-			if (isCII) {
-				nsContext = new CIINapeSpaceResolver();
-			}
-			xPath.setNamespaceContext(nsContext);
+    public PeppolStandardBusinessHeader originalParse(InputStream inputStream) {
+        try {
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(inputStream);
+            if (hasSBDH(document)) {
+                throw new AlreadySBDHException("Document already have SBDH.");
+            }
+            boolean isCII = isCrossIndustryInvoice(document);
+            XPath xPath = XPathFactory.newInstance().newXPath();
+            NamespaceContext nsContext = new HardCodedNamespaceResolver();
+            if (isCII) {
+                nsContext = new CIINapeSpaceResolver();
+            }
+            xPath.setNamespaceContext(nsContext);
 
-			PeppolStandardBusinessHeader sbdh = PeppolStandardBusinessHeader.createPeppolStandardBusinessHeaderWithNewDate();
+            PeppolStandardBusinessHeader sbdh = PeppolStandardBusinessHeader.createPeppolStandardBusinessHeaderWithNewDate();
 
-			// use the plain UBL header parser to decode format and create correct document
-			// parser
-			PlainUBLHeaderParser headerParser = new PlainUBLHeaderParser(document, xPath);
-			if (isCII) {
-				headerParser = new CIIHeaderParser(document, xPath);
-			}
-			// make sure we actually have a UBL type document
-			if (headerParser.canParse()) {
-				sbdh.setDocumentTypeIdentifier(headerParser.fetchDocumentTypeId().toVefa());
-				sbdh.setProfileTypeIdentifier(headerParser.fetchProcessTypeId());
-				// try to use a specialized document parser to fetch more document details
-				PEPPOLDocumentParser documentParser = null;
-				try {
-					String localName = headerParser.localName();
-					if ("CrossIndustryInvoice".equals(localName)) {
-						documentParser = headerParser.createDocumentParser();
-					} else if ("Invoice".equals(localName) || "CreditNote".equals(localName)) {
-						documentParser = new DelisInvoiceDocumentParser(headerParser);
-					}
-				} catch (Exception ex) {
-					/*
+            // use the plain UBL header parser to decode format and create correct document
+            // parser
+            PlainUBLHeaderParser headerParser = new DelisUBLHeaderParser(document, xPath);
+            if (isCII) {
+                headerParser = new CIIHeaderParser(document, xPath);
+            }
+            // make sure we actually have a UBL type document
+            if (headerParser.canParse()) {
+                sbdh.setDocumentTypeIdentifier(headerParser.fetchDocumentTypeId().toVefa());
+                sbdh.setProfileTypeIdentifier(headerParser.fetchProcessTypeId());
+                // try to use a specialized document parser to fetch more document details
+                PEPPOLDocumentParser documentParser = null;
+                try {
+                    String localName = headerParser.localName();
+                    if ("CrossIndustryInvoice".equals(localName)) {
+                        documentParser = headerParser.createDocumentParser();
+                    } else if ("Invoice".equals(localName) || "CreditNote".equals(localName)) {
+                        documentParser = new DelisInvoiceDocumentParser(headerParser);
+                    }
+                } catch (Exception ex) {
+                    /*
 					 * allow this to happen so that "unknown" PEPPOL documents still can be used by
 					 * explicitly setting sender and receiver thru API
 					 */
-				}
-				/*
+                }
+                /*
 				 * However, if we found an eligible parser, we should be able to determine the
 				 * sender and receiver
 				 */
-				if (documentParser != null) {
-					try {
-						sbdh.setSenderId(documentParser.getSender());
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					try {
-						sbdh.setRecipientId(documentParser.getReceiver());
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}
+                if (documentParser != null) {
+                    try {
+                        sbdh.setSenderId(documentParser.getSender());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        sbdh.setRecipientId(documentParser.getReceiver());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
-			return sbdh;
-		} catch (AlreadySBDHException en) {
-			throw en;
-		} catch (Exception e) {
-			throw new RuntimeException("Unable to parseOld document: " + e.getMessage(), e);
-		}
-	}
+            return sbdh;
+        } catch (AlreadySBDHException en) {
+            throw en;
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to parseOld document: " + e.getMessage(), e);
+        }
+    }
 
-	private boolean hasSBDH(Document document) {
-		return "StandardBusinessDocument".equalsIgnoreCase(document.getDocumentElement().getLocalName());
-	}
+    private boolean hasSBDH(Document document) {
+        return "StandardBusinessDocument".equalsIgnoreCase(document.getDocumentElement().getLocalName());
+    }
 
-	private boolean isCrossIndustryInvoice(Document document) {
-		return "CrossIndustryInvoice".equalsIgnoreCase(document.getDocumentElement().getLocalName());
-	}
+    private boolean isCrossIndustryInvoice(Document document) {
+        return "CrossIndustryInvoice".equalsIgnoreCase(document.getDocumentElement().getLocalName());
+    }
 
 }
