9994829e586d57a6bdb20eda04ecff0e8342caa9
Dmitriy Lapko <dlk@truelink.dk>
2019-04-14 12:55:35 +0000

    Implement proper unit test of DocumentValidationTransformationService - with mocked rule repositories and not involved SpringBootTests - for performance of tests

--- delis-web/src/test/java/dk/erst/delis/task/document/process/DocumentValidationTransformationServiceUnitTest.java
+++ delis-web/src/test/java/dk/erst/delis/task/document/process/DocumentValidationTransformationServiceUnitTest.java
@@ -2,19 +2,22 @@ package dk.erst.delis.task.document.process;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
+import dk.erst.delis.TestUtil;
 import dk.erst.delis.config.ConfigBean;
+import dk.erst.delis.config.rule.DefaultRuleBuilder;
 import dk.erst.delis.dao.ConfigValueDaoRepository;
 import dk.erst.delis.dao.RuleDocumentTransformationDaoRepository;
 import dk.erst.delis.dao.RuleDocumentValidationDaoRepository;
@@ -28,23 +31,44 @@ import dk.erst.delis.task.organisation.setup.data.OrganisationReceivingFormatRul
 import dk.erst.delis.web.transformationrule.TransformationRuleService;
 import dk.erst.delis.web.validationrule.ValidationRuleService;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
-public class DocumentValidationTransformationServiceTestIT {
+public class DocumentValidationTransformationServiceUnitTest {
 
-	@Autowired
-	private RuleDocumentTransformationDaoRepository tRuleRepository;
+	private static DocumentValidationTransformationService processService;
 
-	@Autowired
-	private RuleDocumentValidationDaoRepository vRuleRepository;
+	@BeforeClass
+	public static void init() {
+		RuleDocumentTransformationDaoRepository ruleDocumentTransformationDaoRepository = mock(RuleDocumentTransformationDaoRepository.class);
+		when(ruleDocumentTransformationDaoRepository.findAll(any(PageRequest.class))).then(d -> {
+			return new PageImpl<>(DefaultRuleBuilder.buildDefaultTransformationRuleList());
+		});
+		when(ruleDocumentTransformationDaoRepository.findAll()).then(d -> {
+			return DefaultRuleBuilder.buildDefaultTransformationRuleList();
+		});
+		RuleDocumentValidationDaoRepository ruleDocumentValidationDaoRepository = mock(RuleDocumentValidationDaoRepository.class);
+		when(ruleDocumentValidationDaoRepository.findAll(any(PageRequest.class))).then(d -> {
+			return new PageImpl<>(DefaultRuleBuilder.buildDefaultValidationRuleList());
+		});
+		when(ruleDocumentValidationDaoRepository.findAll()).then(d -> {
+			return DefaultRuleBuilder.buildDefaultValidationRuleList();
+		});
+
+		ConfigBean configBean = new ConfigBean(TestUtil.getEmptyConfigValueDaoRepository());
+		TransformationRuleService tRuleService = new TransformationRuleService(ruleDocumentTransformationDaoRepository);
+		ValidationRuleService vRuleService = new ValidationRuleService(ruleDocumentValidationDaoRepository);
+		RuleService ruleService = new RuleService(configBean, vRuleService, tRuleService);
+		DocumentParseService parseService = new DocumentParseService();
+		processService = new DocumentValidationTransformationService(ruleService, parseService);
+
+	}
 	
-	@Autowired
-	private ConfigValueDaoRepository configRepository;
-
 	@Test
 	public void testCII() throws Exception {
 		runCase(TestDocument.CII);
+	}
+	
+	@Test
+	public void testInvoiceResponse() throws Exception {
+		runCase(TestDocument.BIS3_INVOICE_RESPONSE);
 	}
 
 	@Test
@@ -66,13 +90,7 @@ public class DocumentValidationTransformationServiceTestIT {
 	private void runCase(TestDocument testDocument) throws IOException {
 		Path testFile = TestDocumentUtil.createTestFile(testDocument);
 		try {
-			DocumentParseService parseService = new DocumentParseService();
-			ConfigBean configBean = new ConfigBean(configRepository);
-			TransformationRuleService tRuleService = new TransformationRuleService(tRuleRepository);
-			ValidationRuleService vRuleService = new ValidationRuleService(vRuleRepository);
-			RuleService ruleService = new RuleService(configBean, vRuleService, tRuleService);
-			DocumentValidationTransformationService processService = new DocumentValidationTransformationService(ruleService, parseService);
-			
+	
 			Document d = new Document();
 			d.setIngoingDocumentFormat(testDocument.getDocumentFormat());
 			
