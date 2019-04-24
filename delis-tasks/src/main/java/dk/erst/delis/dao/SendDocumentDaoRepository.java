f7fbca33f658eea030fd7c29ee3efee3bf888ae2
dlk <dlk@truelink.dk>
2019-04-24 17:30:13 +0300

    Start big refactoring - move business logic to delis-tasks

--- delis-tasks/src/main/java/dk/erst/delis/dao/SendDocumentDaoRepository.java
+++ delis-tasks/src/main/java/dk/erst/delis/dao/SendDocumentDaoRepository.java
@@ -2,11 +2,12 @@ package dk.erst.delis.dao;
 
 import java.util.List;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
+
 import dk.erst.delis.data.entities.document.SendDocument;
 import dk.erst.delis.data.enums.document.SendDocumentStatus;
-import dk.erst.delis.pagefiltering.persistence.AbstractRepository;
 
-public interface SendDocumentDaoRepository extends AbstractRepository<SendDocument> {
+public interface SendDocumentDaoRepository extends PagingAndSortingRepository<SendDocument, Long> {
 	
 	List<SendDocument> findTop1ByDocumentStatusOrderByIdAsc(SendDocumentStatus documentStatus);
 
