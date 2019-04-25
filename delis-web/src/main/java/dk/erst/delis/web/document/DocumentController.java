aae4ec1521a0361387fdb4b95ac8dab2d7d56798
Andriy.Palval <apal@efisco.net>
2019-04-25 18:06:00 +0300

    Add filtering behavior on Documents page. First step. Works for now, but requires refactoring to be used on other pages.

--- delis-web/src/main/java/dk/erst/delis/web/document/DocumentController.java
+++ delis-web/src/main/java/dk/erst/delis/web/document/DocumentController.java
@@ -1,41 +1,11 @@
 package dk.erst.delis.web.document;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-
-import dk.erst.delis.pagefiltering.response.PageContainer;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.apache.commons.io.FileUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.BodyBuilder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import dk.erst.delis.common.util.StatData;
 import dk.erst.delis.dao.DocumentBytesDaoRepository;
 import dk.erst.delis.data.entities.document.Document;
 import dk.erst.delis.data.enums.document.DocumentStatus;
+import dk.erst.delis.pagefiltering.response.PageContainer;
+import dk.erst.delis.pagefiltering.util.WebRequestUtil;
 import dk.erst.delis.task.document.load.DocumentLoadService;
 import dk.erst.delis.task.document.process.DocumentProcessService;
 import dk.erst.delis.task.document.process.validate.result.ErrorRecord;
@@ -45,6 +15,32 @@ import dk.erst.delis.task.document.response.InvoiceResponseService.InvoiceRespon
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @Slf4j
@@ -73,8 +69,12 @@ public class DocumentController {
 	public String listFilter(Model model, WebRequest webRequest) {
 		PageContainer<Document> pageContainer = documentService.getAll(webRequest);
 		model.addAttribute("documentList", pageContainer);
+		List<Document> items = pageContainer.getItems();
+		List<String> orgNames = items.stream().map(r -> r.getOrganisation().getName()).distinct().sorted().collect(Collectors.toList());
+		model.addAttribute("availableOrganisationNames", orgNames);
 		model.addAttribute("selectedIdList", new DocumentStatusBachUdpateInfo());
 		model.addAttribute("statusList", DocumentStatus.values());
+		model.addAttribute("filterFields", WebRequestUtil.collectFilterParametersFromRequest(webRequest));
 		return "/document/list";
 	}
 
