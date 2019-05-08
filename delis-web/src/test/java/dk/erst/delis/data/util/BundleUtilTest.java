36c8bd1ee6116f5b17077d94d299879d81b16910
funtusthan <funtushan@gmail.com>
2019-05-08 17:07:20 +0300

    add BundleUtilsTest: get all enums the implementations of an interface Named and check current the implementation

--- delis-web/src/test/java/dk/erst/delis/data/util/BundleUtilTest.java
+++ delis-web/src/test/java/dk/erst/delis/data/util/BundleUtilTest.java
@@ -2,21 +2,53 @@ package dk.erst.delis.data.util;
 
 import dk.erst.delis.data.enums.Named;
 import dk.erst.delis.data.enums.document.DocumentType;
+import org.codehaus.plexus.util.StringUtils;
 import org.junit.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AssignableTypeFilter;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
 
 public class BundleUtilTest {
 
-    @Test
-    public void checkBundlesForAllNamed() {
-        check(DocumentType.class);
+    private List<Enum> missName = new ArrayList<>();
 
+    @Test
+    public void checkBundlesForAllNamed() throws ClassNotFoundException {
+        ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+        scanner.addIncludeFilter(new AssignableTypeFilter(Named.class));
+        Set<BeanDefinition> res = scanner.findCandidateComponents(DocumentType.class.getPackage().getName());
+        for (BeanDefinition b : res) {
+            Class<?> c = Class.forName(b.getBeanClassName());
+            check((Class<? extends Enum>) c);
+        }
+
+        if (!missName.isEmpty()) {
+            for (Enum v : missName) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(v.getDeclaringClass().getSimpleName());
+                sb.append('.');
+                sb.append(v.name());
+                sb.append("=");
+                sb.append(StringUtils.capitalizeFirstLetter(v.name().replaceAll("_", "").toLowerCase()));
+                System.out.println(sb.toString());
+            }
+        }
+        assertEquals("Some Named enums miss value in messages.properties", 0, missName.size());
     }
 
     private void check(Class<? extends Enum> d) {
         for (Enum e : d.getEnumConstants()) {
-            System.out.println("e = " + e);
+            Named n = (Named) e;
+            String ename = n.getName();
+            if (ename == null || e.name().equals(ename)) {
+                missName.add(e);
+            }
         }
     }
 }
