4989d3205e1af6f1fa564276ad4fff77935978d9
funtusthan@gmail.com <funtusthan@gmail.com>
2019-06-17 13:51:45 +0300

    refactoring to DocumentDelisWebApiService

--- delis-web-api/src/main/java/dk/erst/delis/rest/controller/content/document/DocumentInvoiceRestController.java
+++ delis-web-api/src/main/java/dk/erst/delis/rest/controller/content/document/DocumentInvoiceRestController.java
@@ -1,23 +1,12 @@
 package dk.erst.delis.rest.controller.content.document;
 
-import dk.erst.delis.data.entities.document.Document;
-import dk.erst.delis.data.entities.document.SendDocument;
 import dk.erst.delis.exception.model.FieldErrorModel;
 import dk.erst.delis.exception.statuses.RestConflictException;
-import dk.erst.delis.exception.statuses.RestNotFoundException;
 import dk.erst.delis.rest.data.response.DataContainer;
-import dk.erst.delis.rest.data.response.SuccessData;
 import dk.erst.delis.rest.data.response.invoice.InvoiceResponseData;
-import dk.erst.delis.task.document.process.log.DocumentProcessStepException;
-import dk.erst.delis.task.document.process.validate.result.ErrorRecord;
-import dk.erst.delis.task.document.response.ApplicationResponseService;
-import dk.erst.delis.web.document.DocumentService;
-import dk.erst.delis.web.document.SendDocumentService;
+import dk.erst.delis.service.content.document.DocumentDelisWebApiService;
 import dk.erst.delis.web.document.ir.InvoiceResponseForm;
-import dk.erst.delis.web.document.ir.InvoiceResponseFormControllerConst;
-import dk.erst.delis.web.document.ir.MessageLevelResponseConst;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -27,10 +16,10 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Min;
-import java.io.*;
-import java.nio.file.Files;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Collections;
-import java.util.List;
 
 @Slf4j
 @Validated
@@ -38,120 +27,34 @@ import java.util.List;
 @RequestMapping("/rest/document")
 public class DocumentInvoiceRestController {
 
-    @Autowired
-    private DocumentService documentService;
-    @Autowired
-    private SendDocumentService sendDocumentService;
-    @Autowired
-    private ApplicationResponseService applicationResponseService;
+    private final DocumentDelisWebApiService documentDelisWebApiService;
+
+    public DocumentInvoiceRestController(DocumentDelisWebApiService documentDelisWebApiService) {
+        this.documentDelisWebApiService = documentDelisWebApiService;
+    }
 
     @GetMapping("/{id}/invoice")
     public ResponseEntity<DataContainer<InvoiceResponseData>> view(@PathVariable @Min(1) long id) {
-        Document document = documentService.getDocument(id);
-        if (document == null) {
-            throw new RestNotFoundException(Collections.singletonList(
-                    new FieldErrorModel("ids", HttpStatus.NOT_FOUND.getReasonPhrase(), "Document is not found")));
-        }
-
-        InvoiceResponseData data = new InvoiceResponseData();
-//        data.setDocument(document);
-//        data.setDocumentStatusList(Arrays.asList(DocumentStatus.values()));
-//        data.setLastJournalList(documentService.getDocumentRecords(document));
-//        data.setErrorListByJournalDocumentIdMap(documentService.getErrorListByJournalDocumentIdMap(document));
-//        data.setDocumentBytes(documentDelisWebApiService.findListDocumentBytesByDocumentId(document.getId()).getItems());
-
-        data.setInvoiceResponseUseCaseList(InvoiceResponseFormControllerConst.useCaseList);
-        data.setInvoiceStatusCodeList(InvoiceResponseFormControllerConst.invoiceStatusCodeList);
-        data.setStatusActionList(InvoiceResponseFormControllerConst.statusActionList);
-        data.setStatusReasonList(InvoiceResponseFormControllerConst.statusReasonList);
-
-        data.setMessageLevelResponseUseCaseList(MessageLevelResponseConst.useCaseList);
-        data.setApplicationResponseTypeCodeList(MessageLevelResponseConst.applicationResponseTypeCodeList);
-        data.setApplicationResponseLineResponseCodeList(MessageLevelResponseConst.applicationResponseLineResponseCodeList);
-        data.setApplicationResponseLineReasonCodeList(MessageLevelResponseConst.applicationResponseLineReasonCodeList);
-
-
-//        applicationResponseFormController.fillModel(model, document);
-
-        return ResponseEntity.ok(new DataContainer<>(data));
+        return ResponseEntity.ok(new DataContainer<>(documentDelisWebApiService.getInvoiceResponseDataByDocument(id)));
     }
 
     @PostMapping("/invoice/generate")
     public ResponseEntity<Object> generateInvoiceResponse(@RequestBody InvoiceResponseForm invoiceResponseForm) {
-        log.info("Generating ApplicationResponse for " + invoiceResponseForm);
-        Document document = documentService.getDocument(invoiceResponseForm.getDocumentId());
-        if (document == null) {
-            throw new RestNotFoundException(Collections.singletonList(
-                    new FieldErrorModel("ids", HttpStatus.NOT_FOUND.getReasonPhrase(), "Document is not found")));
-        }
-
-        boolean success = false;
-        File tempFile = null;
-        try {
-            tempFile = Files.createTempFile("Generated" + invoiceResponseForm.getDocumentFormatName() + "_", ".xml").toFile();
-        } catch (IOException e) {
-            log.error("Failed document generated", e);
-            throw new RestConflictException(Collections.singletonList(
-                    new FieldErrorModel("ids", HttpStatus.CONFLICT.getReasonPhrase(), "Failed document generated: " + e.getMessage())));
-        }
-        try (OutputStream out = new FileOutputStream(tempFile)) {
-            success = applicationResponseService.generateApplicationResponse(document, invoiceResponseForm.getData(), out);
-        } catch (ApplicationResponseService.ApplicationResponseGenerationException | IOException e) {
-            log.error("Failed document generated", e);
-            throw new RestConflictException(Collections.singletonList(
-                    new FieldErrorModel("ids", HttpStatus.CONFLICT.getReasonPhrase(), "Failed document generated: " + e.getMessage())));
-        }
-
-        if (!success) {
-            log.error("Failed to generate " + invoiceResponseForm.getDocumentFormatName());
-            throw new RestConflictException(Collections.singletonList(
-                    new FieldErrorModel("ids", HttpStatus.CONFLICT.getReasonPhrase(), "Failed to generate " + invoiceResponseForm.getDocumentFormatName())));
-        }
-
-        if (invoiceResponseForm.isValidate()) {
-            List<ErrorRecord> errorList;
-            if (invoiceResponseForm.isMessageLevelResponse()) {
-                errorList = applicationResponseService.validateMessageLevelResponse(tempFile.toPath());
-            } else {
-                errorList = applicationResponseService.validateInvoiceResponse(tempFile.toPath());
-            }
-            if (!errorList.isEmpty()) {
-                StringBuilder sb = new StringBuilder();
-                sb.append("Generated ");
-                sb.append(invoiceResponseForm.getDocumentFormatName());
-                sb.append(" is not valid by schema or schematron, found ");
-                sb.append(errorList.size());
-                sb.append(" errors");
-                log.error(sb.toString());
-                throw new RestConflictException(Collections.singletonList(
-                        new FieldErrorModel("ids", HttpStatus.CONFLICT.getReasonPhrase(), sb.toString())));
-            }
-        }
-
-        if (invoiceResponseForm.isGenerateWithoutSending()) {
+        Object o = documentDelisWebApiService.generateInvoiceResponse(invoiceResponseForm);
+        if (o instanceof File) {
+            File file = (File) o;
             ResponseEntity.BodyBuilder resp = ResponseEntity.ok();
             resp.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + invoiceResponseForm.getDocumentFormatName() + ".xml\"");
             resp.contentType(MediaType.parseMediaType("application/octet-stream"));
             try {
-                return resp.body(new InputStreamResource(new FileInputStream(tempFile)));
+                return resp.body(new InputStreamResource(new FileInputStream(file)));
             } catch (FileNotFoundException e) {
                 log.error("Failed to generate without sending", e);
                 throw new RestConflictException(Collections.singletonList(
                         new FieldErrorModel("ids", HttpStatus.CONFLICT.getReasonPhrase(), "Failed to generate without sending: " + e.getMessage())));
             }
-        }
-
-        try {
-            SendDocument sendDocument = sendDocumentService.sendFile(tempFile.toPath(), "Generated by form on document #" + invoiceResponseForm.getDocumentId(), !invoiceResponseForm.isValidate());
-            return ResponseEntity.ok(new DataContainer<>(new SuccessData(true, invoiceResponseForm.getDocumentFormatName() + " with status " + sendDocument.getDocumentStatus())));
-        } catch (DocumentProcessStepException se) {
-            log.error("Failed document processing", se);
-            throw new RestConflictException(Collections.singletonList(
-                    new FieldErrorModel("ids", HttpStatus.CONFLICT.getReasonPhrase(), "Failed to process file " + tempFile + " with error " + se.getMessage())));
-        } catch (Exception e) {
-            log.error("Failed to load file " + tempFile, e);
-            throw new RestConflictException(Collections.singletonList(
-                    new FieldErrorModel("ids", HttpStatus.CONFLICT.getReasonPhrase(), "Failed to load file " + tempFile + " with error " + e.getMessage())));
+        } else {
+            return ResponseEntity.ok(new DataContainer<>(o));
         }
     }
 }
--- delis-web-api/src/main/java/dk/erst/delis/service/content/document/DocumentDelisWebApiServiceImpl.java
+++ delis-web-api/src/main/java/dk/erst/delis/service/content/document/DocumentDelisWebApiServiceImpl.java
@@ -2,6 +2,7 @@ package dk.erst.delis.service.content.document;
 
 import dk.erst.delis.data.entities.document.Document;
 import dk.erst.delis.data.entities.document.DocumentBytes;
+import dk.erst.delis.data.entities.document.SendDocument;
 import dk.erst.delis.data.enums.document.DocumentBytesType;
 import dk.erst.delis.exception.model.FieldErrorModel;
 import dk.erst.delis.exception.statuses.RestConflictException;
@@ -11,10 +12,19 @@ import dk.erst.delis.persistence.repository.document.DocumentBytesRepository;
 import dk.erst.delis.persistence.repository.document.DocumentRepository;
 import dk.erst.delis.rest.data.response.ListContainer;
 import dk.erst.delis.rest.data.response.PageContainer;
+import dk.erst.delis.rest.data.response.SuccessData;
+import dk.erst.delis.rest.data.response.invoice.InvoiceResponseData;
 import dk.erst.delis.service.content.AbstractGenerateDataService;
 import dk.erst.delis.service.security.SecurityService;
+import dk.erst.delis.task.document.process.log.DocumentProcessStepException;
+import dk.erst.delis.task.document.process.validate.result.ErrorRecord;
+import dk.erst.delis.task.document.response.ApplicationResponseService;
 import dk.erst.delis.util.SecurityUtil;
 import dk.erst.delis.web.document.DocumentService;
+import dk.erst.delis.web.document.SendDocumentService;
+import dk.erst.delis.web.document.ir.InvoiceResponseForm;
+import dk.erst.delis.web.document.ir.InvoiceResponseFormControllerConst;
+import dk.erst.delis.web.document.ir.MessageLevelResponseConst;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,8 +35,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.request.WebRequest;
 
-import java.io.ByteArrayOutputStream;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -37,6 +49,8 @@ public class DocumentDelisWebApiServiceImpl implements DocumentDelisWebApiServic
     private final DocumentBytesRepository documentBytesRepository;
     private final SecurityService securityService;
     private final DocumentService documentService;
+    private final SendDocumentService sendDocumentService;
+    private final ApplicationResponseService applicationResponseService;
     private final AbstractGenerateDataService<DocumentRepository, Document> abstractGenerateDataService;
     @Value("${delis.download.allow.all:#{false}}")
     private boolean downloadAllowAll;
@@ -47,12 +61,14 @@ public class DocumentDelisWebApiServiceImpl implements DocumentDelisWebApiServic
             DocumentBytesRepository documentBytesRepository,
             SecurityService securityService,
             DocumentService documentService,
-            AbstractGenerateDataService<DocumentRepository, Document> abstractGenerateDataService) {
+            AbstractGenerateDataService<DocumentRepository, Document> abstractGenerateDataService, SendDocumentService sendDocumentService, ApplicationResponseService applicationResponseService) {
         this.documentRepository = documentRepository;
         this.documentBytesRepository = documentBytesRepository;
         this.securityService = securityService;
         this.documentService = documentService;
         this.abstractGenerateDataService = abstractGenerateDataService;
+        this.sendDocumentService = sendDocumentService;
+        this.applicationResponseService = applicationResponseService;
     }
 
     @Override
@@ -105,6 +121,101 @@ public class DocumentDelisWebApiServiceImpl implements DocumentDelisWebApiServic
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         this.documentService.getDocumentBytesContents(documentBytes, out);
         return out.toByteArray();
+    }
+
+    @Override
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
+    @Transactional(readOnly = true)
+    public InvoiceResponseData getInvoiceResponseDataByDocument(long id) {
+        Document document = documentService.getDocument(id);
+        if (document == null) {
+            throw new RestNotFoundException(Collections.singletonList(
+                    new FieldErrorModel("ids", HttpStatus.NOT_FOUND.getReasonPhrase(), "Document is not found")));
+        }
+
+        InvoiceResponseData data = new InvoiceResponseData();
+
+        data.setInvoiceResponseUseCaseList(InvoiceResponseFormControllerConst.useCaseList);
+        data.setInvoiceStatusCodeList(InvoiceResponseFormControllerConst.invoiceStatusCodeList);
+        data.setStatusActionList(InvoiceResponseFormControllerConst.statusActionList);
+        data.setStatusReasonList(InvoiceResponseFormControllerConst.statusReasonList);
+
+        data.setMessageLevelResponseUseCaseList(MessageLevelResponseConst.useCaseList);
+        data.setApplicationResponseTypeCodeList(MessageLevelResponseConst.applicationResponseTypeCodeList);
+        data.setApplicationResponseLineResponseCodeList(MessageLevelResponseConst.applicationResponseLineResponseCodeList);
+        data.setApplicationResponseLineReasonCodeList(MessageLevelResponseConst.applicationResponseLineReasonCodeList);
+
+        return data;
+    }
+
+    @Override
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
+    public Object generateInvoiceResponse(InvoiceResponseForm invoiceResponseForm) {
+        log.info("Generating ApplicationResponse for " + invoiceResponseForm);
+        Document document = documentService.getDocument(invoiceResponseForm.getDocumentId());
+        if (document == null) {
+            throw new RestNotFoundException(Collections.singletonList(
+                    new FieldErrorModel("ids", HttpStatus.NOT_FOUND.getReasonPhrase(), "Document is not found")));
+        }
+
+        boolean success = false;
+        File tempFile = null;
+        try {
+            tempFile = Files.createTempFile("Generated" + invoiceResponseForm.getDocumentFormatName() + "_", ".xml").toFile();
+        } catch (IOException e) {
+            log.error("Failed document generated", e);
+            throw new RestConflictException(Collections.singletonList(
+                    new FieldErrorModel("ids", HttpStatus.CONFLICT.getReasonPhrase(), "Failed document generated: " + e.getMessage())));
+        }
+        try (OutputStream out = new FileOutputStream(tempFile)) {
+            success = applicationResponseService.generateApplicationResponse(document, invoiceResponseForm.getData(), out);
+        } catch (ApplicationResponseService.ApplicationResponseGenerationException | IOException e) {
+            log.error("Failed document generated", e);
+            throw new RestConflictException(Collections.singletonList(
+                    new FieldErrorModel("ids", HttpStatus.CONFLICT.getReasonPhrase(), "Failed document generated: " + e.getMessage())));
+        }
+
+        if (!success) {
+            log.error("Failed to generate " + invoiceResponseForm.getDocumentFormatName());
+            throw new RestConflictException(Collections.singletonList(
+                    new FieldErrorModel("ids", HttpStatus.CONFLICT.getReasonPhrase(), "Failed to generate " + invoiceResponseForm.getDocumentFormatName())));
+        }
+
+        if (invoiceResponseForm.isValidate()) {
+            List<ErrorRecord> errorList;
+            if (invoiceResponseForm.isMessageLevelResponse()) {
+                errorList = applicationResponseService.validateMessageLevelResponse(tempFile.toPath());
+            } else {
+                errorList = applicationResponseService.validateInvoiceResponse(tempFile.toPath());
+            }
+            if (!errorList.isEmpty()) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("Generated ");
+                sb.append(invoiceResponseForm.getDocumentFormatName());
+                sb.append(" is not valid by schema or schematron, found ");
+                sb.append(errorList.size());
+                sb.append(" errors");
+                log.error(sb.toString());
+                throw new RestConflictException(Collections.singletonList(
+                        new FieldErrorModel("ids", HttpStatus.CONFLICT.getReasonPhrase(), sb.toString())));
+            }
+        }
+
+        if (invoiceResponseForm.isGenerateWithoutSending()) {
+            return tempFile;
+        }
+        try {
+            SendDocument sendDocument = sendDocumentService.sendFile(tempFile.toPath(), "Generated by form on document #" + invoiceResponseForm.getDocumentId(), !invoiceResponseForm.isValidate());
+            return new SuccessData(true, invoiceResponseForm.getDocumentFormatName() + " with status " + sendDocument.getDocumentStatus());
+        } catch (DocumentProcessStepException se) {
+            log.error("Failed document processing", se);
+            throw new RestConflictException(Collections.singletonList(
+                    new FieldErrorModel("ids", HttpStatus.CONFLICT.getReasonPhrase(), "Failed to process file " + tempFile + " with error " + se.getMessage())));
+        } catch (Exception e) {
+            log.error("Failed to load file " + tempFile, e);
+            throw new RestConflictException(Collections.singletonList(
+                    new FieldErrorModel("ids", HttpStatus.CONFLICT.getReasonPhrase(), "Failed to load file " + tempFile + " with error " + e.getMessage())));
+        }
     }
 
     private DocumentBytes findByIdAndDocumentId(Long id, Long bytesId) {
