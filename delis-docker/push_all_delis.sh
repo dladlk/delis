2263953c714db62b91a8805fb9e4ea046c9398c4
dlk <dlk@truelink.dk>
2019-06-18 18:48:47 +0000

    Prepare new version 1.1.1

--- delis-docker/push_all_delis.sh
+++ delis-docker/push_all_delis.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
-NEW_VERSION=1.1.0
+NEW_VERSION=1.1.1
 az acr login --name=tledelivery
 
-docker tag delis-angular:latest					tledelivery.azurecr.io/delis-angular:${NEW_VERSION}
-docker tag delis-web-admin:latest				tledelivery.azurecr.io/delis-web-admin:${NEW_VERSION}
-docker tag delis-web-api:latest					tledelivery.azurecr.io/delis-web-api:${NEW_VERSION}
-docker tag delis-sender-service:latest			tledelivery.azurecr.io/delis-sender-service:${NEW_VERSION}
-docker tag delis-domibus-ws-sender:latest		tledelivery.azurecr.io/delis-domibus-ws-sender:${NEW_VERSION}
+docker tag delis-angular:latest                 tledelivery.azurecr.io/delis-angular:${NEW_VERSION}
+docker tag delis-web-admin:latest               tledelivery.azurecr.io/delis-web-admin:${NEW_VERSION}
+docker tag delis-web-api:latest                 tledelivery.azurecr.io/delis-web-api:${NEW_VERSION}
+docker tag delis-sender-service:latest          tledelivery.azurecr.io/delis-sender-service:${NEW_VERSION}
+docker tag delis-domibus-ws-sender:latest       tledelivery.azurecr.io/delis-domibus-ws-sender:${NEW_VERSION}
 
 docker push tledelivery.azurecr.io/delis-angular:${NEW_VERSION}
 docker push tledelivery.azurecr.io/delis-web-admin:${NEW_VERSION}
