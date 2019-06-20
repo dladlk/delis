179a289ba270d02aaf23809e07065df26629e24a
funtusthan@gmail.com <funtusthan@gmail.com>
2019-06-20 10:41:04 +0300

    remove compact page, Place holder "Please search"/"Venligst søg" replace with "Search" / "Søg"

--- delis-angular/src/app/app.component.ts
+++ delis-angular/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { NgxSpinnerService } from "ngx-spinner";
-import { RuntimeConfigService } from "./service/runtime.config.service";
-import { environment } from '../environments/environment';
-
+import {Component, OnInit} from '@angular/core';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {RuntimeConfigService} from './service/runtime.config.service';
+import {environment} from '../environments/environment';
+import {TranslateService} from '@ngx-translate/core';
+import {LocaleService} from './service/locale.service';
 
 @Component({
     selector: 'app-root',
@@ -13,11 +14,16 @@ export class AppComponent implements OnInit {
 
     version = environment.version;
 
-    constructor(private configService: RuntimeConfigService, private spinner: NgxSpinnerService) {
+    constructor(
+        private configService: RuntimeConfigService,
+        private spinner: NgxSpinnerService,
+        private translate: TranslateService,
+        private locale: LocaleService) {
+        this.translate.use(locale.getlocale().match(/en|da/) ? locale.getlocale() : 'en');
     }
 
     ngOnInit() {
-        let currentVersion = localStorage.getItem('appVersion');
+        const currentVersion = localStorage.getItem('appVersion');
         if (currentVersion === null || currentVersion !== this.version) {
             localStorage.clear();
             localStorage.setItem('appVersion', this.version);
