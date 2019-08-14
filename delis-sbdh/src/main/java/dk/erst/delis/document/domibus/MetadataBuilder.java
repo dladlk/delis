d71f8408dea5733883d8dbd45674a6b76d7ce438
Dmitriy Lapko <dla.own@gmail.com>
2019-08-14 18:11:48 +0300

    Domibus 4.1 has changed XSD for fs-plugin and added PayloadInfo to store there MimeType and PayloadName. Updated XSD and modified MetadataBuilder to set mimeType not via MessageProperty, but via PayloadInfo.

--- delis-sbdh/src/main/java/dk/erst/delis/document/domibus/MetadataBuilder.java
+++ delis-sbdh/src/main/java/dk/erst/delis/document/domibus/MetadataBuilder.java
@@ -1,14 +1,27 @@
 package dk.erst.delis.document.domibus;
 
-import eu.domibus.plugin.fs.ebms3.*;
+import java.util.List;
+
+import eu.domibus.plugin.fs.ebms3.AgreementRef;
+import eu.domibus.plugin.fs.ebms3.CollaborationInfo;
+import eu.domibus.plugin.fs.ebms3.From;
+import eu.domibus.plugin.fs.ebms3.MessageProperties;
+import eu.domibus.plugin.fs.ebms3.PartInfo;
+import eu.domibus.plugin.fs.ebms3.PartProperties;
+import eu.domibus.plugin.fs.ebms3.PartyId;
+import eu.domibus.plugin.fs.ebms3.PartyInfo;
+import eu.domibus.plugin.fs.ebms3.PayloadInfo;
+import eu.domibus.plugin.fs.ebms3.Property;
+import eu.domibus.plugin.fs.ebms3.Service;
+import eu.domibus.plugin.fs.ebms3.UserMessage;
 import no.difi.vefa.peppol.common.model.Header;
 import no.difi.vefa.peppol.common.model.ParticipantIdentifier;
-
-import java.util.List;
 
 public class MetadataBuilder {
 
 	public static String DEFAULT_PROCESS_SCHEME_ID = "urn:fdc:peppol.eu:2017:identifiers:proc-id";
+	
+	private static final boolean PASS_MIME_TYPE_AS_PAYLOAD_INFO = true;
 
 	public UserMessage buildUserMessage(Header sbdhHeader, String partyIdValue) {
 		UserMessage userMessage = new UserMessage();
@@ -24,8 +37,6 @@ public class MetadataBuilder {
 		AgreementRef agreementRef = new AgreementRef();
 		agreementRef.setValue("urn:fdc:peppol.eu:2017:agreements:tia:ap_provider");
 		Service service = new Service();
-//		service.setType(sbdhHeader.getProcess().getScheme().getIdentifier());
-//		service.setType("urn:fdc:peppol.eu:2017:identifiers:proc-id");
 		service.setType(DEFAULT_PROCESS_SCHEME_ID);
 		service.setValue(sbdhHeader.getProcess().getIdentifier());
 		CollaborationInfo collaborationInfo = new CollaborationInfo();
@@ -45,18 +56,38 @@ public class MetadataBuilder {
 		finalRecipient.setType(receiver.getScheme().getIdentifier());
 		finalRecipient.setValue(receiver.getIdentifier());
 
-		Property mimeType = new Property();
-		mimeType.setName("MimeType");
-		mimeType.setValue("text/xml");
-
 		MessageProperties messageProperties = new MessageProperties();
 		List<Property> propertyList = messageProperties.getProperty();
-		propertyList.add(mimeType);
+
+		if (PASS_MIME_TYPE_AS_PAYLOAD_INFO) {
+			PayloadInfo payloadInfo = buildPayloadInfo("cid:message", "MimeType", "text/xml");
+			userMessage.setPayloadInfo(payloadInfo);
+		} else {
+			Property mimeType = new Property();
+			mimeType.setName("mimeType");
+			mimeType.setValue("text/xml");
+			propertyList.add(mimeType);
+		}
+
 		propertyList.add(originalSender);
 		propertyList.add(finalRecipient);
 		userMessage.setPartyInfo(partyInfo);
 		userMessage.setCollaborationInfo(collaborationInfo);
 		userMessage.setMessageProperties(messageProperties);
 		return userMessage ;
+	}
+
+	private PayloadInfo buildPayloadInfo(String href, String propertyName, String propertyValue) {
+		PayloadInfo payloadInfo = new PayloadInfo();
+		PartInfo partInfo = new PartInfo();
+		partInfo.setHref(href);
+		PartProperties partProperties = new PartProperties();
+		Property p = new Property();
+		p.setName(propertyName);
+		p.setValue(propertyValue);
+		partProperties.getProperty().add(p);
+		partInfo.setPartProperties(partProperties);
+		payloadInfo.getPartInfo().add(partInfo);
+		return payloadInfo;
 	}
 }
