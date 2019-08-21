67d3d2bfdc95be7db74e854d2d8da7977238bfa0
funtusthan@gmail.com <funtusthan@gmail.com>
2019-08-21 12:22:42 +0300

    Change dashboard data shown, it should include: Number of sent/ received documents, number of errors of received documents. If something is 0 - do not highlight it

--- delis-angular/src/app/model/content/dashboard.model.ts
+++ delis-angular/src/app/model/content/dashboard.model.ts
@@ -3,4 +3,10 @@ export class DashboardModel {
   errorLastHour: number;
   receivedDocumentsLastHour: number;
   sendDocumentsLastHour: number;
+
+  constructor() {
+    this.errorLastHour = 0;
+    this.receivedDocumentsLastHour = 0;
+    this.sendDocumentsLastHour = 0;
+  }
 }
