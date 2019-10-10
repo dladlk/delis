1342913d7b810dc64003eacc3e07074baf74be2c
dlk <dlk@truelink.dk>
2019-10-10 07:42:07 +0000

    Switch Domibus to send emails directly to SendGrid using account data, stored in local-only yaml file

--- delis-test-server/helm/install-domibus1-test.sh
+++ delis-test-server/helm/install-domibus1-test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -ex
 CURDIR=`dirname $0`
-helm install --name dtx ${CURDIR}/domibus/ --set partyName=Blue --set partyIndex=1 --values ${CURDIR}/install-domibus1-values-4.1.0.yaml --dry-run --debug
+helm install --name dtx ${CURDIR}/domibus/ --set partyName=Blue --set partyIndex=1 --values ${CURDIR}/install-domibus1-values-4.1.0.yaml \
+    --values ${CURDIR}/install-domibus1-values-4.1.0.yaml \
+    --values ${CURDIR}/local-only/install-domibus-local-only.yaml \
+    --dry-run --debug
