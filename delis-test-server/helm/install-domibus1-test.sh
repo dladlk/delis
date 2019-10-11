77b92ce3f7fb0a07d2ee7e81389e6f513f2d4b02
dlk <dlk@truelink.dk>
2019-10-11 10:03:39 +0000

    Modify Htlm chart for DEV to support external JMX

--- delis-test-server/helm/install-domibus1-test.sh
+++ delis-test-server/helm/install-domibus1-test.sh
@@ -3,4 +3,5 @@ CURDIR=`dirname $0`
 helm install --name dtx ${CURDIR}/domibus/ --set partyName=Blue --set partyIndex=1 --values ${CURDIR}/install-domibus1-values-4.1.0.yaml \
     --values ${CURDIR}/install-domibus1-values-4.1.0.yaml \
     --values ${CURDIR}/local-only/install-domibus-local-only.yaml \
+    --values ${CURDIR}/install-domibus1-values-jmx-ext.yaml \
     --dry-run --debug
--- delis-test-server/helm/install-domibus1.sh
+++ delis-test-server/helm/install-domibus1.sh
@@ -2,4 +2,6 @@
 CURDIR=`dirname $0`
 helm upgrade dt1 --install --force ${CURDIR}/domibus/ --set partyName=Blue --set partyIndex=1 \
     --values ${CURDIR}/install-domibus1-values-4.1.0.yaml \
-    --values ${CURDIR}/local-only/install-domibus-local-only.yaml
+    --values ${CURDIR}/local-only/install-domibus-local-only.yaml \
+    --values ${CURDIR}/install-domibus1-values-jmx-ext.yaml
+
