064cd55e87cf6a2482155804c11ed9f4c5ab0fcf
dlk <dlk@truelink.dk>
2019-12-24 09:17:03 +0000

    Save dry-run execution to file with curtime suffix

--- delis-test-server/helm/install-prometheus-test.sh
+++ delis-test-server/helm/install-prometheus-test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -ex
 CURDIR=`dirname $0`
+CURTIME="$(date '+%Y.%m.%d_%H.%M.%S')"
+
 helm upgrade monitoring stable/prometheus-operator --version=6.18.0 --namespace monitoring --install \
     --values ${CURDIR}/install-prometheus.yaml \
-    --dry-run --debug > ./intall-prometheus-test.txt
+    --dry-run --debug > ./intall-prometheus-test-${CURTIME}.txt
 
