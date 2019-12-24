aa29f7043251af747ee693f3e17d207015208cff
dlk <dlk@truelink.dk>
2019-12-24 09:11:42 +0000

    Fix delis-sender-service build script - missed changes check on own folder

--- delis-test-server/build-system/scripts/delis-sender-service.sh
+++ delis-test-server/build-system/scripts/delis-sender-service.sh
@@ -2,7 +2,7 @@
 
 PROJECT_ROOT=/wsh/delis
 PROJECT_CODE=delis-sender-service
-PROJECT_SCAN_PATH_LIST="/delis-sbdh;/delis-data;/delis-tasks;/delis-xslt-util;/delis-oxalis-sender"
+PROJECT_SCAN_PATH_LIST="/delis-sbdh;/delis-data;/delis-tasks;/delis-xslt-util;/delis-oxalis-sender;/delis-sender-service"
 DOCKER_REBUILD_COMMAND=${PROJECT_ROOT}/delis-docker/delis-web/build-${PROJECT_CODE}.sh
 FORCE_UPDATE=
 if [ -z $1 ]; then
