1c66903025e12182b6001c17ce661a924b9b6047
dlk <dlk@truelink.dk>
2019-12-24 09:26:09 +0000

    Enable Prometheus persistance, add 2 more monitors for web-api and sender-service, add smtp integration via local file config

--- delis-test-server/helm/install-prometheus.sh
+++ delis-test-server/helm/install-prometheus.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -ex
 CURDIR=`dirname $0`
 helm upgrade monitoring stable/prometheus-operator --version=8.3.3 --namespace monitoring --install \
-    --values ${CURDIR}/install-prometheus.yaml
-
-# --version=6.18.0
-# --version=8.3.3
+    --values ${CURDIR}/install-prometheus.yaml \
+    --values ${CURDIR}/local-only/install-prometheus-smtp.yaml
