36db6b8d9aaa0576190c933181af6593e82cdfc0
dlk <dlk@truelink.dk>
2019-12-24 09:17:51 +0000

    Upgrade Prometheus-operator to latest version

--- delis-test-server/helm/install-prometheus.sh
+++ delis-test-server/helm/install-prometheus.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 CURDIR=`dirname $0`
-helm upgrade monitoring stable/prometheus-operator --version=6.18.0 --namespace monitoring --install \
+helm upgrade monitoring stable/prometheus-operator --version=8.3.3 --namespace monitoring --install \
     --values ${CURDIR}/install-prometheus.yaml
 
+# --version=6.18.0
+# --version=8.3.3
