504eef68be35205ffbfa1c08bb20fe6f931dc396
Dmitriy Lapko <dla.own@gmail.com>
2020-04-01 19:21:35 +0300

    Reflect dependency on delis-domibus-util at cef-erst-test-web-console

--- delis-test-server/build-system/scripts/cef-erst-test-web-console.sh
+++ delis-test-server/build-system/scripts/cef-erst-test-web-console.sh
@@ -2,7 +2,7 @@
 
 PROJECT_ROOT=/wsh/cef-erst
 PROJECT_CODE=test-web-console
-PROJECT_SCAN_PATH_LIST="/cef-test-web;../delis/delis-domibus-util"
+PROJECT_SCAN_PATH_LIST="/cef-test-web;/../delis/delis-domibus-util"
 DOCKER_REBUILD_COMMAND=${PROJECT_ROOT}/docker/build-test-web.sh
 
 ./zzz_check_changes.sh ${PROJECT_ROOT} ${PROJECT_CODE} ${PROJECT_SCAN_PATH_LIST} ${DOCKER_REBUILD_COMMAND}
