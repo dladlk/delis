7c486595e1c8dde650f19b58cd0a4b32c48fc0da
Dmitriy Lapko <dla.own@gmail.com>
2020-04-10 18:43:27 +0300

    Fix issue with busdox-docid-qns prefix in action (document identifier) - substruct it similar to iso6523-actorid-upis:: prefix in final receipient.

--- delis-docker/domibus-tomcat/erst-domibus-fs-plugin/src/main/java/eu/domibus/plugin/fs/validation/ReceiverSubmissionValidator.java
+++ delis-docker/domibus-tomcat/erst-domibus-fs-plugin/src/main/java/eu/domibus/plugin/fs/validation/ReceiverSubmissionValidator.java
@@ -1,23 +1,25 @@
 package eu.domibus.plugin.fs.validation;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
 import eu.domibus.api.property.DomibusPropertyProvider;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import eu.domibus.plugin.Submission;
 import eu.domibus.plugin.validation.SubmissionValidationException;
 import eu.domibus.plugin.validation.SubmissionValidator;
-import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.Collection;
-import java.util.Date;
-import java.util.stream.Collectors;
 
 @Component("customCefValidator")
 public class ReceiverSubmissionValidator implements SubmissionValidator {
@@ -96,30 +98,60 @@ public class ReceiverSubmissionValidator implements SubmissionValidator {
 
         String finalRecipient = messageProperties.stream().filter(x -> x.getKey().equals("finalRecipient")).findFirst().get().getValue();
 		LOG.info("finalRecipient = " + finalRecipient);
-		if (finalRecipient != null) {
-			/*
-			 * Convert finalRecipient iso6523-actorid-upis::0088:5798009882875 to 0088:5798009882875 
-			 */
-			int startValue = finalRecipient.indexOf("::");
-			if (startValue >= 0 && finalRecipient.length() > startValue + 2) {
-				finalRecipient = finalRecipient.substring(startValue + 2);
-				LOG.info("finalRecipient transformed to " + finalRecipient);
-			}
-		}
+		/*
+		 * Convert finalRecipient iso6523-actorid-upis::0088:5798009882875 to 0088:5798009882875 
+		 */
+		finalRecipient = removeSchemaPrefix(finalRecipient, "finalRecipient", null);
         String service = submission.getService();
         LOG.info("service = " + service);
+		/*
+		 * Convert service 
+		 * 
+		 * nes-procid-ubl::urn:www.nesubl.eu:profiles:profile5:ver2.0 to 
+		 * 
+		 *                 urn:www.nesubl.eu:profiles:profile5:ver2.0 and 
+		 *                 
+		 * cenbii-procid-ubl::urn:fdc:peppol.eu:2017:poacc:billing:01:1.0 to 
+		 * 
+		 *                 urn:fdc:peppol.eu:2017:poacc:billing:01:1.0  
+		 */
+        service = removeSchemaPrefix(service, "service", null);
         String action = submission.getAction();
         LOG.info("action = " + action);
+        /*
+         * Convert action only if busdox-docid-qns::
+         */
+        action = removeSchemaPrefix(action, "action", "busdox-docid-qns");
 
         StringBuilder sb = new StringBuilder();
-        sb.append(encode(finalRecipient)).append(PARAMETER_SEPARATOR)
-                .append(encode(service)).append(PARAMETER_SEPARATOR)
-                .append(encode(action));
+        sb.append(encode(finalRecipient));
+        sb.append(PARAMETER_SEPARATOR);
+        sb.append(encode(service));
+        sb.append(PARAMETER_SEPARATOR);
+        sb.append(encode(action));
 
         return sb.toString();
     }
 
+	private String removeSchemaPrefix(String value, String fieldName, String prefixToRemove) {
+		if (value == null) {
+			return value;
+		}
+		int startValue = value.indexOf("::");
+		if (startValue >= 0 && value.length() > startValue + 2) {
+			String prefix = value.substring(startValue);
+			if (prefixToRemove == null || prefixToRemove.equals(prefix)) {
+				value = value.substring(startValue + 2);
+				LOG.info("Field "+fieldName +" transformed to " + value);
+			}
+		}
+		return value;
+	}
+
     private String encode (String source) throws UnsupportedEncodingException {
-        return URLEncoder.encode(source, UTF_8);
+    	/*
+    	 * RestTemplate ALREADY encodes URL, if we encode it here - it is DOUBLE encoded... But let's keep it
+    	 */
+    	return URLEncoder.encode(source, UTF_8);
     }
 }
