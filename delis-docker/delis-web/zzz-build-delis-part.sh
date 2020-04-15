703eb9f3f30e29dbc02cbce35a81f06993910e22
Dmitriy Lapko <dla.own@gmail.com>
2020-04-15 15:36:06 +0300

    Optimize docker image size by splitting single "COPY ./app $WORK_DIR" on several steps, which start from coping resources with lowest change probability to most often changed parts of unpacked jar. Split coping all jars from oxalis and delis

--- delis-docker/delis-web/zzz-build-delis-part.sh
+++ delis-docker/delis-web/zzz-build-delis-part.sh
@@ -41,6 +41,9 @@ fi
 rm -r -f ./docker/app
 unzip ${DELIS_PART_PATH}/target/${DELIS_PART}-${DELIS_PART_VERSION}.jar -d ./docker/app
 
+mkdir ./docker/app/delis  && mv ./docker/app/BOOT-INF/lib/delis-*.jar ./docker/app/delis
+mkdir ./docker/app/oxalis && mv ./docker/app/BOOT-INF/lib/Oxalis-*.jar ./docker/app/oxalis
+
 DOCKER_IMAGE_NAME=${DELIS_PART}
 
 if [ "${DOCKER_IMAGE_NAME}" = "delis-web" ]; then
