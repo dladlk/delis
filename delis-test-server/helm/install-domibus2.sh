eb8a843311016433ce38eaa28e624db674ab82a0
dlk <dlk@truelink.dk>
2021-11-06 14:50:45 +0000

    Changes on test server to apply new 4.2.5 Domibus

--- delis-test-server/helm/install-domibus2.sh
+++ delis-test-server/helm/install-domibus2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -ex
 CURDIR=`dirname $0`
-helm tiller run helm upgrade dt2 --install --force ${CURDIR}/domibus/ --set partyName=Red --set partyIndex=2 --values ${CURDIR}/install-domibus2-values-4.1.0.yaml
+helm tiller run helm upgrade dt2 --install --force ${CURDIR}/domibus/ \
+     --set partyName=Red \
+     --set partyIndex=2 \
+     --values ${CURDIR}/install-domibus2-values.yaml
 #--dry-run --debug
