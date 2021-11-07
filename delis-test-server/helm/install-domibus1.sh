44ba74e7acd4a2501d1f4a900507a3afcb24da2d
dlk <dlk@truelink.dk>
2021-11-07 10:42:24 +0000

    Disable CRL check by HTTP protocol to overcome concept test PKI with CRL url. Add new property to solve strange requirement for Peppol role in PMode for receiver

--- delis-test-server/helm/install-domibus1.sh
+++ delis-test-server/helm/install-domibus1.sh
@@ -5,6 +5,7 @@ helm tiller run helm upgrade dt1 --install --force ${CURDIR}/domibus/ \
     --set partyName=Blue \
     --set partyIndex=1 \
     --values ${CURDIR}/install-domibus1-values.yaml \
+    --values ${CURDIR}/install-domibus-both-values.yaml \
     --values ${CURDIR}/local-only/install-domibus-local-only.yaml \
     --values ${CURDIR}/install-domibus1-values-jmx-ext.yaml
 
--- delis-test-server/helm/install-domibus2.sh
+++ delis-test-server/helm/install-domibus2.sh
@@ -3,5 +3,6 @@ CURDIR=`dirname $0`
 helm tiller run helm upgrade dt2 --install --force ${CURDIR}/domibus/ \
      --set partyName=Red \
      --set partyIndex=2 \
+     --values ${CURDIR}/install-domibus-both-values.yaml \
      --values ${CURDIR}/install-domibus2-values.yaml
 #--dry-run --debug
