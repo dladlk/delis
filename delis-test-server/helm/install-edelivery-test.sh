3f8c08153caf7f6ee784a914cd5821f63b8954b3
dlk <dlk@truelink.dk>
2022-12-03 11:06:53 +0000

    Changed 20.11.2022 to use minikube context

--- delis-test-server/helm/install-edelivery-test.sh
+++ delis-test-server/helm/install-edelivery-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 CURDIR=`dirname $0`
-helm tiller run helm upgrade edelivery-test --install \
-	--force ${CURDIR}/edelivery-test/ \
+kubectl config use-context minikube
+helm tiller run helm upgrade edelivery-test --install --force ${CURDIR}/edelivery-test/ \
 	--values ${CURDIR}/local-only/install-edelivery-test.yaml
 
